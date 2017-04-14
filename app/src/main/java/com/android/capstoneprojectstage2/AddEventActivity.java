23f62d1fb89184c99ce1df08581c64e130d272f5
Tarun <trikha.tarun01@gmail.com>
2017-04-14 14:30:30 +0530

    working add event layout

--- app/src/main/java/com/android/capstoneprojectstage2/AddEventActivity.java
+++ app/src/main/java/com/android/capstoneprojectstage2/AddEventActivity.java
@@ -1,18 +1,17 @@
 package com.android.capstoneprojectstage2;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by trikh on 14-04-2017.
  */
 
-public class AddEventActivity extends Activity {
+public class AddEventActivity extends AppCompatActivity {
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.add_event_layout);
     }
 }
